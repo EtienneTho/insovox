02f1f83815cf9b28cd8f0d9dcee1b6e944bfe894
EtienneTho <etiennethoret@gmail.com>
2024-05-29 09:18:26 +0200

    Add files via upload

--- 01_acoustic_analyses/01_acoustic_analysis_per_subject.py
+++ 01_acoustic_analyses/01_acoustic_analysis_per_subject.py
@@ -4,6 +4,7 @@ import pickle
 import matplotlib.pyplot as plt  
 import functions
 import scipy.io as sio
+from lib import proise_v2
 from sklearn.model_selection import cross_val_score, StratifiedKFold, cross_val_predict, GridSearchCV, train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, balanced_accuracy_score
@@ -41,7 +42,11 @@ nbScales = 8
 
 # per subject
 tabIndex = np.unique(tabSubjectNb)
-
+allCanonicalMaps = []
+training_accuracies_mean = []
+training_accuracies_std  = []
+testing_accuracies_mean  = []
+testing_accuracies_std   = []
 for index in tabIndex:
   print('Subject nb '+str(index))
   X = []
@@ -75,7 +80,7 @@ for index in tabIndex:
   y_test  = np.concatenate((Y[Y=='2'],Y[Y=='4']))
   
   y_train[y_train=='1'] = 0
-  y_train[y_train=='3'] = 1
+  y_train[y_train=='3'] = 1    
   y_test[y_test=='2']   = 0
   y_test[y_test=='4']   = 1
 
@@ -86,10 +91,10 @@ for index in tabIndex:
 
   # print(X_train.shape)
   # print(y_test.shape)
-  n_cv = 10
+  n_cv = 5
   cv = StratifiedKFold(n_cv, shuffle=True)
   
-  n_components = 10
+  n_components = 40
   print('____number of PCA components: '+str(n_components))
   pca = PCA(n_components=n_components, svd_solver='auto', whiten=True).fit(X_train)
   X_train_pca = pca.transform(np.asarray(X_train))
@@ -110,14 +115,25 @@ for index in tabIndex:
   print()
 
   ##### Second strategy
-  # classification: train on session 1 and 3 and test on session 2 and 4
+  # classification: train / test split with 5-fold cv
   print("Second strategy: 5-fold cross val on all samples (same as PLoS Comp Biol)")
   n_fold = 10
   accTest = []
   accTrain = []
+  canonicalMapsFolds = []
   for fold in range(n_fold):
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=fold)
+    y_train[y_train=='1'] = 0
+    y_train[y_train=='2'] = 0    
+    y_train[y_train=='3'] = 1
+    y_train[y_train=='4'] = 1   
 
+    y_test[y_test=='1']   = 0
+    y_test[y_test=='2']   = 0    
+    y_test[y_test=='3']   = 1
+    y_test[y_test=='4']   = 1    
+
+    print(np.unique(y_train))
     pca = PCA(n_components=n_components, svd_solver='auto', whiten=True).fit(X_train)   
     X_train_pca = pca.transform(np.asarray(X_train))
 
@@ -132,10 +148,41 @@ for index in tabIndex:
     y_pred = clf.predict(pca.transform(X_test))    
     accTest.append(balanced_accuracy_score(y_test,y_pred))
 
-  print('____training accuracy: '+str(np.nanmean(accTrain))+'+/-'+str(np.nanstd(accTrain)))    
-  print('____testing accuracy: '+str(np.nanmean(accTest))+'+/-'+str(np.nanstd(accTrain)))
+    ################################################################################################################################
+    # interpretation stim+pseudo-noise
+    N = 5
+    dimOfinput = (128,8,14) # dimension of the input representation
+    probingMethod = 'revcor' # choice of the probing method : bubbles or revcor
+    samplesMethod = 'pseudoRandom' # choice of the method to generate probing samples trainSet or pseudoRandom (need x_test_set) or gaussianNoise
+    nbRevcorTrials = X_train.shape[0]*N # number of probing samples for the reverse correlation (must be below the number of training sample if trainSet)
+    nDim_pca = 30 # number of dimension to compute the PCA for the pseudo-random noise generation
+    probingSamples, _ = proise_v2.generateProbingSamples(x_train_set = X_train, x_test_set = X_train, dimOfinput=dimOfinput, probingMethod = probingMethod, samplesMethod = samplesMethod, nDim_pca = nDim_pca, nbRevcorTrials = nbRevcorTrials)
+    print(probingSamples.shape)
+    X_probingSamples_pca = pca.transform(probingSamples/2 + np.tile(X_train,(N,1))/2)  
+    y_pred = clf.predict(X_probingSamples_pca)
+    print(np.unique(y_pred))
+    responses_ = np.squeeze((y_pred == np.tile(y_train,(1,N))) & (np.tile(y_train,(1,N)) == '1'))
 
+    canonicalMap = []
+    pval = []
+
+    data2revcor = np.asarray(probingSamples)
+
+    canonicalMap = np.nanmean(data2revcor[responses_][:],axis=0) - np.nanmean(data2revcor[np.logical_not(responses_)][:],axis=0)
+    canonicalMapsFolds.append(canonicalMap)
+  allCanonicalMaps.append(np.mean(np.asarray(canonicalMapsFolds),axis=0))
+  print('____training accuracy: '+str(np.nanmean(accTrain))+'+/-'+str(np.nanstd(accTrain)))    
+  print('____testing accuracy: '+str(np.nanmean(accTest))+'+/-'+str(np.nanstd(accTest)))
   print()
+
+  training_accuracies_mean.append(np.nanmean(accTrain))
+  testing_accuracies_mean.append(np.nanmean(accTest))
+  training_accuracies_std.append(np.nanstd(accTrain))
+  testing_accuracies_std.append(np.nanstd(accTrain))
+
+sio.savemat(outFolder+'withinCanonicalMaps.mat', {'allCanonicalMaps': np.asarray(allCanonicalMaps), 'training_accuracies_mean':np.asarray(training_accuracies_mean), 'training_accuracies_std':np.asarray(training_accuracies_std), 'testing_accuracies_mean':np.asarray(testing_accuracies_mean), 'testing_accuracies_std':np.asarray(testing_accuracies_std)})
+pickle.dump({'allCanonicalMaps': np.asarray(allCanonicalMaps), 'training_accuracies_mean':np.asarray(training_accuracies_mean), 'training_accuracies_std':np.asarray(training_accuracies_std), 'testing_accuracies_mean':np.asarray(testing_accuracies_mean), 'testing_accuracies_std':np.asarray(testing_accuracies_std)}, open(outFolder+'withinCanonicalMaps.pkl', 'wb'))
+
 
 
 
